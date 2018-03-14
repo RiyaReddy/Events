3106264fec06d1f62fb12f80189a67502ff11b9e
RiyaReddy <32082852+RiyaReddy@users.noreply.github.com>
2018-03-14 11:37:50 +1100

    first commit

---
+++ Events/Video.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Events
+{
+    class Video
+    {
+        public string Title { get; set; }
+    }
+}
---
+++ Events/VideoEncoder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Events
+{
+    class VideoEncoder
+    {
+        /*  Steps to handle events
+         1. Define delegate
+         2. Define an event based on that delegate
+         3. Raise the event
+         */
+
+        /* Old way of defining delegate and Event
+            * Define delegate
+             public delegate void VideoEncodedEventHandler(object source, EventArgs args);
+
+            * Define an event based on above delegate
+            public event VideoEncodedEventHandler VideoEncoded;
+        */
+
+        //Good way of defining delegate which acts as bridge between publisher and subscriber
+        public event EventHandler VideoEncoded;
+
+        public void Encode(Video video)
+        {
+            Console.WriteLine("Encoding Video....");
+            Thread.Sleep(3000);
+
+            //Raise the event
+            OnVideoEncoded();
+        }
+
+        protected virtual void OnVideoEncoded()
+        {
+            if (VideoEncoded != null)
+                VideoEncoded(this, EventArgs.Empty);
+        }
+    }
+}
